3c36b25786a5df0752dca934502e112961460dbc
Richard Knoll <riknoll@users.noreply.github.com>
2021-06-17 12:33:30 -0700

    Initial files for MakeCode project

---
+++ test.ts
@@ -0,0 +1,49 @@
+const allImages = [spritePackSprites.mySprite,spritePackSprites.mySprite2,spritePackSprites.mySprite3,spritePackSprites.mySprite4,spritePackSprites.mySprite5,spritePackSprites.mySprite6,spritePackSprites.mySprite7,spritePackSprites.mySprite8,spritePackSprites.mySprite9,spritePackSprites.mySprite10,spritePackSprites.mySprite11,spritePackSprites.mySprite12,spritePackSprites.mySprite13,spritePackSprites.mySprite14,spritePackSprites.mySprite15,spritePackSprites.mySprite16,spritePackSprites.mySprite17,spritePackSprites.mySprite18,spritePackSprites.mySprite19,spritePackSprites.mySprite20,spritePackSprites.mySprite21,spritePackSprites.mySprite22,spritePackSprites.mySprite23,spritePackSprites.mySprite24,spritePackSprites.mySprite25,spritePackSprites.mySprite26,spritePackSprites.mySprite27,spritePackSprites.mySprite28,spritePackSprites.mySprite29,spritePackSprites.mySprite30,spritePackSprites.mySprite31,spritePackSprites.mySprite32,spritePackSprites.mySprite33,spritePackSprites.mySprite34,spritePackSprites.mySprite35,spritePackSprites.mySprite36,spritePackSprites.mySprite37,spritePackSprites.mySprite38,spritePackSprites.mySprite39,spritePackSprites.mySprite40,spritePackSprites.mySprite41,spritePackSprites.mySprite42,spritePackSprites.mySprite43,spritePackSprites.mySprite44,spritePackSprites.mySprite45,spritePackSprites.mySprite46,spritePackSprites.mySprite47,spritePackSprites.mySprite48,spritePackSprites.mySprite49,spritePackSprites.mySprite50,spritePackSprites.mySprite51,spritePackSprites.mySprite52,spritePackSprites.mySprite53,spritePackSprites.mySprite54,spritePackSprites.mySprite55,spritePackSprites.mySprite56,spritePackSprites.mySprite57,spritePackSprites.mySprite58,spritePackSprites.mySprite59,spritePackSprites.mySprite60,spritePackSprites.mySprite61,spritePackSprites.mySprite62,spritePackSprites.mySprite63,spritePackSprites.mySprite64,spritePackSprites.mySprite65,spritePackSprites.mySprite66,spritePackSprites.mySprite67,spritePackSprites.mySprite68,spritePackSprites.mySprite69,spritePackSprites.mySprite70,spritePackSprites.mySprite71,spritePackSprites.mySprite72,spritePackSprites.mySprite73,spritePackSprites.mySprite74,spritePackSprites.mySprite75,spritePackSprites.mySprite76,spritePackSprites.mySprite77,spritePackSprites.mySprite78,spritePackSprites.mySprite79,spritePackSprites.mySprite80,spritePackSprites.mySprite81,spritePackSprites.mySprite82,spritePackSprites.mySprite83,spritePackSprites.mySprite84,spritePackSprites.mySprite85,spritePackSprites.mySprite86,spritePackSprites.mySprite87,spritePackSprites.mySprite88,spritePackSprites.mySprite89,spritePackSprites.mySprite90,spritePackSprites.mySprite91,spritePackSprites.mySprite92,spritePackSprites.mySprite93,spritePackSprites.mySprite94,spritePackSprites.mySprite95,spritePackSprites.mySprite96,spritePackSprites.mySprite97,spritePackSprites.mySprite98,spritePackSprites.mySprite99,spritePackSprites.mySprite100,spritePackSprites.mySprite101,spritePackSprites.mySprite102,spritePackSprites.mySprite103,spritePackSprites.mySprite104,spritePackSprites.mySprite105,spritePackSprites.mySprite106,spritePackSprites.mySprite107,spritePackSprites.mySprite108,spritePackSprites.mySprite109,spritePackSprites.mySprite110,spritePackSprites.mySprite111,spritePackSprites.mySprite112,spritePackSprites.mySprite113,spritePackSprites.mySprite114,spritePackSprites.mySprite115,spritePackSprites.mySprite116,spritePackSprites.mySprite117,spritePackSprites.mySprite118,spritePackSprites.mySprite119,spritePackSprites.mySprite120,spritePackSprites.mySprite121,spritePackSprites.mySprite122,spritePackSprites.mySprite123,spritePackSprites.mySprite124,spritePackSprites.mySprite125,spritePackSprites.mySprite126,spritePackSprites.mySprite127,spritePackSprites.mySprite128,spritePackSprites.mySprite129,spritePackSprites.mySprite130,spritePackSprites.mySprite131,spritePackSprites.mySprite132,spritePackSprites.mySprite133,spritePackSprites.mySprite134,spritePackSprites.mySprite135,spritePackSprites.mySprite136,spritePackSprites.mySprite137,spritePackSprites.mySprite138,spritePackSprites.mySprite139,spritePackSprites.mySprite140,spritePackSprites.mySprite141,spritePackSprites.mySprite142,spritePackSprites.mySprite143,spritePackSprites.mySprite144,spritePackSprites.mySprite145,spritePackSprites.mySprite146,spritePackSprites.mySprite147,spritePackSprites.mySprite148,spritePackSprites.mySprite149,spritePackSprites.mySprite150,spritePackSprites.mySprite151,spritePackSprites.mySprite152,spritePackSprites.mySprite153,spritePackSprites.mySprite154,spritePackSprites.mySprite155,spritePackSprites.mySprite156,spritePackSprites.mySprite157,spritePackSprites.mySprite158,spritePackSprites.mySprite159,spritePackSprites.mySprite160,spritePackSprites.mySprite161,spritePackSprites.mySprite162,spritePackSprites.mySprite163,spritePackSprites.mySprite164,spritePackSprites.mySprite165,spritePackSprites.mySprite166,spritePackSprites.mySprite167,spritePackSprites.mySprite168,spritePackSprites.mySprite169,spritePackSprites.mySprite170,spritePackSprites.mySprite171,spritePackSprites.mySprite172,spritePackSprites.mySprite173,spritePackSprites.mySprite174,spritePackSprites.mySprite175,spritePackSprites.mySprite176,spritePackSprites.mySprite177,spritePackSprites.mySprite178,spritePackSprites.mySprite179,spritePackSprites.mySprite180,spritePackSprites.mySprite181,spritePackSprites.mySprite182,spritePackSprites.mySprite183,spritePackSprites.mySprite184,spritePackSprites.mySprite185,spritePackSprites.mySprite186,spritePackSprites.mySprite187,spritePackSprites.mySprite188,spritePackSprites.mySprite189,spritePackSprites.mySprite190,spritePackSprites.mySprite191,spritePackSprites.mySprite192,spritePackSprites.mySprite193,spritePackSprites.mySprite194,spritePackSprites.mySprite195,spritePackSprites.mySprite196,spritePackSprites.mySprite197,spritePackSprites.mySprite198,spritePackSprites.mySprite199,spritePackSprites.mySprite200,spritePackSprites.mySprite201,spritePackSprites.mySprite202,spritePackSprites.mySprite203,spritePackSprites.mySprite204,spritePackSprites.mySprite205,spritePackSprites.mySprite206,spritePackSprites.mySprite207,spritePackSprites.mySprite208,spritePackSprites.mySprite209,spritePackSprites.mySprite210]
+
+const padding = 10;
+const speed = 50;
+game.onUpdate(function() {
+    for (const sprite of sprites.allOfKind(SpriteKind.Player)) {
+        if (sprite.vx > 0 && sprite.x >= screen.width - padding) {
+            sprite.x = screen.width - padding;
+            sprite.vx = 0;
+            sprite.vy = speed;
+        }
+        else if (sprite.vy > 0 && sprite.y >= screen.height - padding) {
+            sprite.y = screen.height - padding;
+            sprite.vx = -speed;
+            sprite.vy = 0;
+        }
+        else if (sprite.vx < 0 && sprite.x <= padding) {
+            sprite.x = padding;
+            sprite.vx = 0;
+            sprite.vy = -speed;
+        }
+        else if (sprite.vy < 0 && sprite.bottom <= 0) {
+            sprite.destroy();
+        }
+    }
+})
+
+let index = 0;
+game.onUpdateInterval(700, function() {
+    const asset = sprites.create(allImages[index], SpriteKind.Player);
+    asset.x = padding;
+    asset.y = padding;
+    asset.vx = speed;
+    asset.setFlag(SpriteFlag.Ghost, true)
+    index = (index + 1) % allImages.length;
+})
+
+let line1 = sprites.create(img`0`, SpriteKind.Food)
+line1.say("PRESS A TO  ")
+
+let line2 = sprites.create(img`0`, SpriteKind.Food)
+line2.say("CHANGE COLOR")
+line2.top += 10
+
+let bgColor = 0;
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function() {
+    scene.setBackgroundColor(bgColor);
+    bgColor = (bgColor + 1) % 15
+})
